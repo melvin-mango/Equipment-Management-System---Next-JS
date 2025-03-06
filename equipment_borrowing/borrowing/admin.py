8a867b14e78914916e8111e3fa1346000833a5e2
Bradley <97529026+BradKingori@users.noreply.github.com>
2025-03-06 12:02:14 +0300

    Delete equipment_borrowing directory

--- equipment_borrowing/borrowing/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Equipment, BorrowRequest
-
-admin.site.register(Equipment)
-admin.site.register(BorrowRequest)
--- equipment_borrowing/borrowing/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.dashboard, name='home'),  # Default view for the borrowing app
-    path('login/', views.custom_login, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('logout/', views.logout_view, name='logout'),
-]
