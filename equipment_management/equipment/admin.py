a7995efcc714966b1df9c38b891ddd5e472075e5
melvin-mango <melvinlewisusiu@gmail.com>
2025-03-15 11:49:11 +0300

    Removed ems_front-end from Git tracking

--- equipment_management/equipment/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Student,BorrowRequest,Equipment
-
-admin.site.register(Equipment)
-admin.site.register(BorrowRequest)
-admin.site.register(Student)
-# Register your models here.
